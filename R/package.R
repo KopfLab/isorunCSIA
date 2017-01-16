b8c2bc52ce98906f5327182e362f807fa387b670
Sebastian Kopf <seb.kopf@gmail.com>
2017-01-15 18:52:04 -0700

    basic package structure

---
+++ R/package.R
@@ -0,0 +1,14 @@
+#' @title Run compound specific isotope analysis
+#' @description Test
+#' @name isorunCSIA
+#' @docType package
+#' @author Sebastian Kopf
+#' @import dplyr ggplot2
+#' @importFrom tidyr gather
+#' @importFrom isoread isoread
+#' @importFrom methods is
+#' @importFrom stats setNames
+#' @importFrom stringr str_match_all
+#'
+#' @include loading.R
+NULL
