6729bb3d10be9327d1f9f005f2d60e9a2b803bf2
Sebastian Kopf <seb.kopf@gmail.com>
2017-02-07 21:44:37 -0700

    file browser module updated with recent file

--- inst/gui/module_file_browser.R
+++ inst/gui/module_file_browser.R
@@ -1,4 +1,6 @@
 library(shinyBS)
+library(magrittr)
+library(dplyr)
 
 #' fileSelector
 #' @param allow_upload whether to allow upload
@@ -15,7 +17,7 @@ fileSelectorInput <- function(id, allow_upload = FALSE, upload_label = NULL) {
       tags$script(
         "
         $('#%s').on('click', function(){
-          Shiny.onInputChange('%s', Math.random());
+        Shiny.onInputChange('%s', Math.random());
         })" %>%
         sprintf(ns("tab_path"), ns("tab_click"))),
 
@@ -23,27 +25,36 @@ fileSelectorInput <- function(id, allow_upload = FALSE, upload_label = NULL) {
       tags$script(
         "
         $('#%s').on('dblclick', function(){
-          var obj = $('select#%s')[0];
-          Shiny.onInputChange('%s', obj.options[obj.selectedIndex].value);
-          Shiny.onInputChange('%s', Math.random());
+        var obj = $('select#%s')[0];
+        Shiny.onInputChange('%s', obj.options[obj.selectedIndex].value);
+        Shiny.onInputChange('%s', Math.random());
         })" %>%
         sprintf(ns("content_list"), ns("content_list"),
                 ns("content_double_click_item"), ns("content_double_click")))
     )
 
   return(dialog_tags)
-}
+  }
 
 #' modalFileSelector
+#' @param open_label the label for the link to open the modal dialog
+#' @param dialog_label the label of the modal dialog, same as the open_label by default
+#' @param close_label the label for the button to close the modal dialog
+#' @param size the size of the dialog, default is small
 #' @param ... parameters for fileSelectorInput
-modalFileSelectorInput <- function(id, label = "Select file", link_wrapper = identity, ...) {
+modalFileSelectorInput <- function(id, open_label = "Select file", dialog_label = open_label, size = "large", close_label = "Select", link_wrapper = identity, ...) {
   ns <- NS(id)
+  modal_dlg <- bsModal(ns("modal_dialog"), dialog_label, ns("modal_link"), size = size,
+                       column(width = 12, fileSelectorInput(id, ...)) %>% fluidRow())
+
+  modal_dlg$children[[1]]$children[[1]]$children[[3]]$children[[1]]$children[[1]] <- close_label
+  modal_dlg$children[[1]]$children[[1]]$children[[3]]$children[[1]]$attribs$id <- ns("close")
+  modal_dlg$children[[1]]$children[[1]]$children[[3]]$children[[1]]$attribs$class <- "btn btn-default action-button"
+
   dialog_tags <-
     tagList(
-      link_wrapper(actionLink(ns("modal_link"), label, icon = icon("file"))),
-      bsModal(ns("modal_dialog"), label, ns("modal_link"), size = "small",
-              column(width = 12, fileSelectorInput(id, ...)) %>% fluidRow()
-      )
+      link_wrapper(actionLink(ns("modal_link"), open_label, icon = icon("file"))),
+      modal_dlg
     )
   return(dialog_tags)
 }
@@ -52,11 +63,16 @@ modalFileSelectorInput <- function(id, label = "Select file", link_wrapper = ide
 #' @param root_name the root directory name
 #' @param sort_desc whether to sort in descending order
 #' @param pattern regexp file selection pattern
+#' @param multiple whether to allow multiple file selection
+#' @param enable_recent whether to have a recent tab
+#' @param number_recent how many recent items to show
+#' @param exclude_recent regex what to explude in the recent list (by default nothing is excluded)
 #' @param size number of rows in the selection box
 fileSelector <- function(input, output, session,
-                         root, root_name = basename(root),
+                         root, root_name = basename(root), size = 8,
                          sort_desc = FALSE, pattern = NULL, multiple = TRUE,
-                         size = 8) {
+                         enable_recent = TRUE, start_recent = TRUE, number_recent = 10,
+                         exclude_recent = "%^") {
 
   # namespace
   ns <- session$ns
@@ -67,8 +83,9 @@ fileSelector <- function(input, output, session,
     last_double_click_item = "",
     current_directory = root,
     current_directory_content = NULL,
-    content_list = c()
-    )
+    content_list = c(),
+    recent_active = FALSE
+  )
 
   # uploads
   observe({
@@ -96,23 +113,33 @@ fileSelector <- function(input, output, session,
 
   # update reactive value current_directory
   observe({
-
     # refresh whenever directory tab is clicked or subfolder is double clicked
     input$tab_click
     input$content_double_click
 
     # isolate everything after
     isolate({
-      # base path (start at root if not set yet)
+      # base path (start at root if not set yet, unless recent_active parameter is TRUE)
       path <- input$tab_path
-      if (is.null(path)) path <- root
+      if (is.null(path)) {
+        if (start_recent) path <- "<<RECENT>>"
+        else path <- root
+      }
+
+      # check if recent is selected
+      if (path == "<<RECENT>>") {
+        if (!values$recent_active) {
+          values$recent_active <- TRUE
+        }
+        return() # done
+      }
 
       # see if last double click item is a valid subdirectory
       subdir <- values$last_double_click_item
       if (!is.null(subdir) && subdir != "" &&
           dirname(subdir) == path && # make sure it's a subdirectory
           dir.exists(subdir) # make sure it's a directory
-        ) {
+      ) {
         path <- subdir
         values$last_double_click_item <- "" # reset
       }
@@ -122,9 +149,10 @@ fileSelector <- function(input, output, session,
       if (!R.utils::isAbsolutePath(path)) stop("not an absolute path: ", path)
 
       # update reactive value
-      if (path != values$current_directory) {
+      if (values$recent_active || path != values$current_directory) {
         values$current_directory <- path
         values$current_directory_content <- NULL
+        values$recent_active <- FALSE
       }
     })
   })
@@ -132,40 +160,63 @@ fileSelector <- function(input, output, session,
   # update reactive value content_list
   observe({
 
-      # trigger when current folder changes and check for updates in that folder every second
-      invalidateLater(1000, session)
+    # triggers if directory changes or if recent is activated
+    # triggers when current folder changes (checks for updates every second)
+    invalidateLater(1000, session)
+
+    if (values$recent_active) {
+      # only show recent files (recursive from root)
+      new_content <-
+        list.files(root, pattern = pattern, full.names = T, recursive = TRUE) %>%
+        lapply(function(file) list(file = file, mtime = file.mtime(file))) %>%
+        bind_rows() %>%
+        filter(!grepl(exclude_recent, file)) %>%
+        arrange(desc(mtime)) %>%
+        filter(row_number() <= number_recent)
+      old_content <- isolate(values$current_directory_content)
+      if (!is.null(old_content) && setdiff(new_content$mtime, old_content) %>% length() == 0)
+        return() # okay, nothing changed
+
+      # update content list (stores the mtime vector)
+      values$current_directory_content <- new_content$mtime
+      message("INFO: (Re)loading recent files (NS: ", ns(NULL),")")
+      values$content_list <- setNames(new_content$file,
+                                      sub("^[\\/]?", "",
+                                          sub(root, "", new_content$file, fixed = TRUE)))
+
+    } else {
+      # content of selected folder (files and subfolders)
       path <- values$current_directory
-      # just a quick content comparison (no full paths or anything)
       new_content <- c(list.dirs(path, rec = FALSE), list.files(path, pattern = pattern))
       old_content <- isolate(values$current_directory_content)
-      if (!is.null(old_content) && setdiff(new_content, old_content) %>% length() == 0) {
+      if (!is.null(old_content) && setdiff(new_content, old_content) %>% length() == 0)
         return() # okay, nothing changed
-      }
 
-      # something changed --> update contents list
-      message("INFO: (Re)loading folder: ", path, " (NS: ", ns(NULL),")")
+      # update contents list (stores the filename vector)
       values$current_directory_content <- new_content
+      message("INFO: (Re)loading folder: ", path, " (NS: ", ns(NULL),")")
       folders <- list.dirs(path, rec=FALSE, full.names = T) # w/ full names
       files <- setdiff(list.files(path, full.names = T, pattern = pattern), folders)
-
       # sort in descending order if asked for
       if (sort_desc) {
         folders <- rev(folders)
         files <- rev(files)
       }
 
-      # combine
+      # update content list
       values$content_list <-
         setNames(
           c(folders, files),
           c(sprintf("[ %s ]", folders %>% sapply(basename)), files %>% sapply(basename))
         )
-    })
+    }
+  })
 
   # generate path tabs
   output$tab_path <- renderUI({
     tmp_path <- values$current_directory
-    parents <- list(id = ns("tab_path"), selected = tmp_path)
+    selected_tab <- if (enable_recent && isolate(values$recent_active)) "<<RECENT>>" else tmp_path
+    parents <- list(id = ns("tab_path"), selected = selected_tab)
     while (tmp_path != dirname(root)){
       if (tmp_path == root)
         parent <- root_name
@@ -173,6 +224,10 @@ fileSelector <- function(input, output, session,
         parent <- basename(tmp_path)
       parents <- c(parents, list(tabPanel(parent, value = tmp_path)))
       tmp_path <- dirname(tmp_path)
+    }
+    if (enable_recent) {
+      # make recent bullet
+      parents <- c(parents, list(tabPanel("Recent", value = "<<RECENT>>")))
     }
     do.call(tabsetPanel, args = parents[length(parents):1])
   })
@@ -186,9 +241,14 @@ fileSelector <- function(input, output, session,
 
   # return both the current path and the selected folder contents
   list(
+    modal_closed = reactive(input$close),
     path = reactive(values$current_directory),
+    path_relative = reactive(sub("^[\\/]?", "", sub(root, "", values$current_directory, fixed = TRUE))),
     selection = reactive(input$content_list),
+    selection_relative = reactive(sub("^[\\/]?", "", sub(root, "", input$content_list, fixed = TRUE))),
     double_click = reactive(values$last_double_click_item)
   )
 }
+
+
 
